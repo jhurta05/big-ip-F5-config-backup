8c24b75f4e4b84881456cdfcfd6b114dc49dd6d7
Jesus Hurtado <124899894+jhurta05@users.noreply.github.com>
2024-11-20 13:39:06 -0600

    Update backupscript.sh

--- backupscript.sh
+++ backupscript.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
+#Creates a backup config file and put it in the /var/ucs/ directory.
 tmsh save /sys ucs /var/ucs/$(/bin/hostname)-$(date +\%Y-\%m-\%d).ucs
 
-## Replace the <remote_user_name>, <sftp_IP_address> and <sftp_directory> as needed
-HOST="10.111.1.158"
-USERNAME="wanguy"
-PASSWORD="Sandw1chDques01"
+## Replace the YOUR_USERNAME, HOST_IP_OR_NAME and YOUR_PASSWORD as needed:
+HOST="HOST_IP_OR_NAME"
+USERNAME="YOUR_USERNAME"
+PASSWORD="YOUR_PASSWORD"
 
-#sftp $USERNAME@$HOST <<EOF
-#cd /Users/wanguy/Documents/F5_Backups/
-#put /var/ucs/$(/bin/hostname)-$(date +\%Y-\%m-\%d).ucs
-#bye
-#EOF
-
+#This section will create the connection to the SFTP server using the credentials provided above, change directory 
+#to the one where you want the file to be stored and put(upload) the backupt file into the the SFTP server.
 expect <<EOF
 spawn sftp $USERNAME@$HOST
 expect "Password:"
 send "$PASSWORD\n"
 expect "sftp>"
-send "cd /Users/wanguy/Documents/F5_Backups/\n"
+send "cd /Users/SFTP_SERVER_FOLDER_PATH/\n"
 expect "sftp>"
 send "put /var/ucs/$(/bin/hostname)-$(date +\%Y-\%m-\%d).ucs\n"
 expect "sftp>"
@@ -27,13 +24,13 @@ interact
 EOF
 
 
-## Optional: Uncomment the following line to delete the ucs file
+## Optional: Uncomment the following line to delete the ucs file from the F5
 tmsh delete /sys ucs /var/ucs/$(/bin/hostname)-$(date +\%Y-\%m-\%d).ucs
 
 
 
 #crontab -e
-#15 4 * * 0 /home/jhurta/backupscript.sh    ##Runs Weekly every Sunday at 4:15AM
+#15 4 * * 0 /home/tasks/backupscript.sh    ##Runs Weekly every Sunday at 4:15AM
 #crontab -l
 #touch backupscript.sh
 #nano backupscript.sh
