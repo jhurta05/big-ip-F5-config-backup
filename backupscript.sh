e85139297574680cd61c898fc1d6ff3c12753a2d
Jesus Hurtado <124899894+jhurta05@users.noreply.github.com>
2024-11-21 10:50:05 -0600

    Update backupscript.sh

--- backupscript.sh
+++ backupscript.sh
@@ -8,8 +8,8 @@ HOST="HOST_IP_OR_NAME"
 USERNAME="YOUR_USERNAME"
 PASSWORD="YOUR_PASSWORD"
 
-#This section will create the connection to the SFTP server using the credentials provided above, change directory 
-#to the one where you want the file to be stored and put(upload) the backupt file into the the SFTP server.
+## This section will create the connection to the SFTP server using the credentials provided above, change directory 
+## to the one where you want the file to be stored and put(upload) the backupt file into the the SFTP server.
 expect <<EOF
 spawn sftp $USERNAME@$HOST
 expect "Password:"
@@ -25,13 +25,4 @@ EOF
 
 
 ## Optional: Uncomment the following line to delete the ucs file from the F5
-tmsh delete /sys ucs /var/ucs/$(/bin/hostname)-$(date +\%Y-\%m-\%d).ucs
-
-
-
-#crontab -e
-#15 4 * * 0 /home/tasks/backupscript.sh    ##Runs Weekly every Sunday at 4:15AM
-#crontab -l
-#touch backupscript.sh
-#nano backupscript.sh
-#chmod +x backupscript.sh
+#tmsh delete /sys ucs /var/ucs/$(/bin/hostname)-$(date +\%Y-\%m-\%d).ucs
